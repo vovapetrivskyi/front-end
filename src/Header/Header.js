bde906a0673655f5de52c31c835f15769be770aa
vovapetrivskyi@ukr.net <vovapetrivskyi@ukr.net>
2023-09-30 20:57:08 +0300

    header with links

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 function Header() {
   return (
     <div className="header">
       <h1 class="centeredTextAlign">Doggos site</h1>
+      <nav className="centeredTextAlign">
+            <Link to="/" className="link">Home</Link>
+            <Link to="/AddEdit" className="link">Add Doggo</Link>
+      </nav>
     </div>
   );
 }
